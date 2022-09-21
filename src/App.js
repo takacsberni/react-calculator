7786f4e697370c81dcb6a0bbaa8ac86ce88c4e56
takacsberni <takacsberni@gmail.com>
2022-09-21 18:07:04 +0200

    feat: create the calculator's body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  return (
+      <div className="container">
+          <div className="display"></div>
+          <div className="buttons">
+              <button className="btn-number">1</button>
+              <button className="btn-number">2</button>
+              <button className="btn-number">3</button>
+              <button className="btn-operator">+</button>
+              <button className="btn-number">4</button>
+              <button className="btn-number">5</button>
+              <button className="btn-number">6</button>
+              <button className="btn-operator">-</button>
+              <button className="btn-number">7</button>
+              <button className="btn-number">8</button>
+              <button className="btn-number">9</button>
+              <button className="btn-operator">X</button>
+              <button className="btn-clear">C</button>
+              <button className="btn-number">0</button>
+              <button className="btn-equals">=</button>
+              <button className="btn-operator">/</button>
+          </div>
+      </div>
   );
 }
 
