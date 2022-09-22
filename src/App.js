5351f7ac18b0a9825e33bf09b2e70e7579690898
takacsberni <takacsberni@gmail.com>
2022-09-22 11:46:05 +0200

    feat: display is now an input field + create handleClick which helps to show the clicked buttons name

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import './App.css';
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+    const [result, setResult] = useState("");
 
-  return (
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name));
+        console.log(e.target.name)
+    }
+    return (
       <div className="container">
-          <div className="display"></div>
+          <input className="display" value={result}></input>
           <div className="buttons">
-              <button className="btn-number">1</button>
-              <button className="btn-number">2</button>
-              <button className="btn-number">3</button>
-              <button className="btn-operator">+</button>
-              <button className="btn-number">4</button>
-              <button className="btn-number">5</button>
-              <button className="btn-number">6</button>
-              <button className="btn-operator">-</button>
-              <button className="btn-number">7</button>
-              <button className="btn-number">8</button>
-              <button className="btn-number">9</button>
-              <button className="btn-operator">X</button>
-              <button className="btn-clear">C</button>
-              <button className="btn-number">0</button>
-              <button className="btn-equals">=</button>
-              <button className="btn-operator">/</button>
+              <button className="btn-number" id="clear">1</button>
+              <button className="btn-number" name="2" onClick={handleClick}>2</button>
+              <button className="btn-number" name="3" onClick={handleClick}>3</button>
+              <button className="btn-operator" name="+" onClick={handleClick}>+</button>
+              <button className="btn-number" name="4" onClick={handleClick}>4</button>
+              <button className="btn-number" name="5" onClick={handleClick}>5</button>
+              <button className="btn-number" name="6" onClick={handleClick}>6</button>
+              <button className="btn-operator" name="-" onClick={handleClick}>-</button>
+              <button className="btn-number" name="7" onClick={handleClick}>7</button>
+              <button className="btn-number" name="8" onClick={handleClick}>8</button>
+              <button className="btn-number" name="9" onClick={handleClick}>9</button>
+              <button className="btn-operator" name="X" onClick={handleClick}>X</button>
+              <button className="btn-clear" name="C" onClick={handleClick}>C</button>
+              <button className="btn-number" name="0" onClick={handleClick}>0</button>
+              <button className="btn-equals" name="=" onClick={handleClick}>=</button>
+              <button className="btn-operator" name="/" onClick={handleClick}>/</button>
           </div>
       </div>
   );
